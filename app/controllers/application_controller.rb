ad9a9f64f98bd2fb12ee2913aedaedecbec7c19c
Nicole Pupillo <nicole.pupillo@gmail.com>
2015-03-06 18:15:33 -0500

    Commented out root node setting: root node is now true

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,8 @@ class ApplicationController < ActionController::API
   include ActionController::Serialization
 
   #make root node false, globally
-  def default_serializer_options
-    {root: false}
-  end
+  # def default_serializer_options
+  #   {root: false}
+  # end
 
 end
