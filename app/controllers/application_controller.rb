6d97e38e632c5281c7e76cc6776d248f4a908ebb
Nicole Pupillo <nicole.pupillo@gmail.com>
2015-03-06 15:36:31 -0500

    Set global default to make root node false

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::API
   include ActionController::Serialization
+
+  #make root node false, globally
+  def default_serializer_options
+    {root: false}
+  end
+
 end
